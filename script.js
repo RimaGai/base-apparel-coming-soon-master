e07f88c81c6cdba60034905866faba0ea73ca5ed
RimaGai <puolus@gmail.com>
2023-06-19 18:20:48 +0200

    Add email validation

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-let email = document.getElementById("email").value;
-let button = document.getElementsByClassName("button");
+const form = document.querySelector("form");
+const errorMessage = document.querySelector(".error-message");
+const errorIcon = document.querySelector(".error-icon");
+const input = document.querySelector("#email");
 
-
-function isEmpty() {
-  if (email == "") {
-   
+function checkEmail() {
+  const emailValue = input.value.trim();
+  if (emailValue === "" || !validate(emailValue)) {
+    errorMessage.classList.remove("hidden");
+    errorIcon.classList.remove("hidden");
+  } else {
+    errorMessage.classList.add("hidden");
+    errorIcon.classList.add("hidden");
   }
 }
+
+function validate(email) {
+  let re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  checkEmail();
+});
